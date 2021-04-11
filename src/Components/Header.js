275e98452083c71013bee921049e4ab9bd2b292c
yashsehgal <yashsehgal.work@gmail.com>
2021-04-12 02:32:27 +0530

    header component added

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <a src="" className="logo">GetResume.io</a>
+      <div className="header_options">
+        <a href="https://www.github.com/DesignSystemsOSS/getresume.io">GitHub</a>
+      </div>
+    </div>
+  )
+};
+
+export default Header;
