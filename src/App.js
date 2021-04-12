99eaf72c98d6ee1975b1c0318bd35cf0385a00d5
yashsehgal <yashsehgal.work@gmail.com>
2021-04-12 12:36:23 +0530

    homepage created (1/2)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,79 @@
 import Header from './Components/Header';
+import Footer from './Components/Footer';
 import './css/main.css';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <div id="landing-section" className="section">
+        <h1 className="center">Get Perfect Resumes in <br /> just one click</h1>
+        <button className="dark center-button">Create your first Resume <i className="fas fa-arrow-right" id="arrow-in-button"></i></button>
+        {/* Demo Resume Container */}
+        <div className="demo_resume">
+          <div id="demo-resume-header">
+            <h2 id="demo-name">John Doe</h2>
+            <p id="demo-title">Software Developer. UI Designer</p>
+            <div id="social-icons">
+              <i className="fab fa-github"></i>
+              <i className="fab fa-linkedin"></i>
+              <i className="fab fa-behance"></i>
+              <i className="fab fa-twitter"></i>
+              <i className="far fa-envelope"></i>
+            </div>
+          </div>
+          <div className="demo-resume-inner-content">
+            <div id="skills-section">
+              <h2 id="demo-skill-title">Skills</h2>
+              <p id="demo-skills">Python, JavaScript, C/C++, HTML, CSS/SCSS</p>
+              <p id="demo-skills">Frontend, JSON, Git/GitHub</p>
+            </div>  {/** End of Skills Container */}
+            <div id="demo-educational-details">
+              <h2 id="demo-educational-title">Education</h2>
+              <div id="demo-education-container">
+                <h3>Bachelors of Technology (Computer Science and Engineering)</h3>
+                <p id="demo-college-name">By, University Name</p>
+              </div>
+            </div>
+          </div>
+        </div>    {/* End of Demo Resume Container */}
+
+        <div id="features-section" className="section">
+          <div id="inner-feature-section">
+            <h2>Easy PDF Downloading feature. Create and Download <br />in PDF Format.</h2>
+            <p className="description left">
+              Create resumes which are pleasant in design and looks good to the recruiters. 
+              You can select from various color options and themes, layouts and download for 
+              free. You just need to log in first.
+            </p><br /><br /><br /><br /><br />
+            <button className="dark">Create your first Resume <i className="fas fa-arrow-right" id="arrow-in-button"></i></button>
+          </div>
+          <div id="inner-feature-section">
+            <div className="demo-card">
+              <div className="flex">
+                <i className="fas fa-file-pdf"></i>
+                <i className="fas fa-download"></i>
+              </div>
+              <b>
+                <p id="demo-card-title">Save as PDF</p>
+              </b>
+            </div> {/** End of Demo Card */}
+          </div>
+        </div>    {/** End of Features Section */}
+
+        {/* Notification Section for Open Source Contribution */}
+        <div className="notification">
+          <h2>Want to Contribute to this project? 👀</h2>
+          <p>Well, I should tell you that this project is open sourced under The DesignSystems Community. Feel free, It's your project!
+            Visit the GitHub Repository of this project and read the contribution guidelines and Let's get started! 👍
+          </p>
+          <div id="button_layer">
+            <button className="dark">Check GitHub Source Code</button>
+            <button className="light">Read CONTRIBUTING.md <i className="fas fa-arrow-right" id="arrow-in-button-dark"></i></button>
+          </div>
+        </div>
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
