e419c378a7c51a9925e4b1190e40a4f3a5042ab6
yashsehgal <yashsehgal.work@gmail.com>
2021-04-12 12:35:59 +0530

    components added (Header, Footer)

---
+++ src/Components/Footer.js
@@ -0,0 +1,56 @@
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <div className="Footer-upper-section">
+        <div id="footer-option-list">
+          <h5 className="light-text">Open Source</h5>
+          <ul type="none">
+            <li>
+              <a href="" className="footer-option">GitHub</a>
+            </li>
+            <li>
+              <a href="" className="footer-option">Community Guidelines</a>
+            </li>
+            <li>
+              <a href="" className="footer-option">The DesignSystems</a>
+            </li>
+            <li>
+              <a href="" className="footer-option">Contribution</a>
+            </li>
+            <li>
+              <a href="" className="footer-option">License</a>
+            </li>
+          </ul>
+        </div>
+
+        <div id="footer-option-list">
+          <h5 className="light-text">The DesignSystems</h5>
+          <ul type="none">
+            <li>
+              <a href="" className="footer-option">GitHub</a>
+            </li>
+            <li>
+              <a href="" className="footer-option">Community Guidelines</a>
+            </li>
+            <li>
+              <a href="" className="footer-option">Eccentric Touch</a>
+            </li>
+            <li>
+              <a href="" className="footer-option">Docs</a>
+            </li>
+            <li>
+              <a href="" className="footer-option">License</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="Footer-lower-section">
+
+      </div>
+    </div>
+  )
+};
+
+export default Footer;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,8 @@ const Header = () => {
     <div className="Header">
       <a src="" className="logo">GetResume.io</a>
       <div className="header_options">
-        <a href="https://www.github.com/DesignSystemsOSS/getresume.io">GitHub</a>
+        <a href="" className="option">Create Resume</a>
+        <a href="https://www.github.com/DesignSystemsOSS/getresume.io" className="option" target="_blank">GitHub</a>
       </div>
     </div>
   )
