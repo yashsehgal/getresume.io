b0536e07dc73f067217e14ec551874592ef82256
yashsehgal <yashsehgal.work@gmail.com>
2021-04-14 02:40:23 +0530

    Header Components updated, paths are modified

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,10 +2,10 @@
 const Header = () => {
   return (
     <div className="Header">
-      <a href="/home" className="logo">GetResume.io</a>
+      <a href="/getresume.io/" className="logo">GetResume.io</a>
       <div className="header_options">
-        <a href="/getresume.io/home" className="option">Home</a>
-        <a href="" className="option">Create Resume</a>
+        <a href="/getresume.io/" className="option">Home</a>
+        <a href="/getresume.io/create-resume" className="option">Create Resume</a>
         <a href="https://www.github.com/DesignSystemsOSS/getresume.io" className="option" target="_blank">GitHub</a>
         <a href="/getresume.io/login" className="option">Login</a>
         <a href="/getresume.io/signup" className="option">Create an Account</a>
