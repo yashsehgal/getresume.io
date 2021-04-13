9f811886884f7fe5a62e16cfa8dbc351ec1279ae
yashsehgal <yashsehgal.work@gmail.com>
2021-04-13 13:57:44 +0530

    new pages added for Login/Signup Activites

---
+++ src/pages/LoginActivity.js
@@ -0,0 +1,12 @@
+
+import '../css/LoginActivityStyle.css';
+
+const LoginActivity = () => {
+  return (
+    <div className="LoginActivity">
+      <p>Running component LoginActivity</p>
+    </div>
+  )
+};
+
+export default LoginActivity;
---
+++ src/pages/SignupActivity.js
@@ -0,0 +1,10 @@
+
+import '../css/SignupActivityStyle.css';
+
+const SignupActivity = () => {
+  <div className="SignupActivity">
+
+  </div>
+};
+
+export default SignupActivity;
