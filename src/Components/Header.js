28753f168399363ab7322952c4956e14cf57e320
yashsehgal <yashsehgal.work@gmail.com>
2021-04-14 00:47:54 +0530

    components updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,11 +4,11 @@ const Header = () => {
     <div className="Header">
       <a href="/home" className="logo">GetResume.io</a>
       <div className="header_options">
-        <a href="/home" className="option">Home</a>
+        <a href="/getresume.io/home" className="option">Home</a>
         <a href="" className="option">Create Resume</a>
         <a href="https://www.github.com/DesignSystemsOSS/getresume.io" className="option" target="_blank">GitHub</a>
-        <a href="/login" className="option">Login</a>
-        <a href="/signup" className="option">Create an Account</a>
+        <a href="/getresume.io/login" className="option">Login</a>
+        <a href="/getresume.io/signup" className="option">Create an Account</a>
       </div>
     </div>
   )
