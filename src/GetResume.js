fc87eebe34014c1916c57524de135027eb419a52
yashsehgal <yashsehgal.work@gmail.com>
2021-05-06 14:00:10 +0530

    main app screen modified

--- src/GetResume.js
+++ src/GetResume.js
@@ -7,19 +7,21 @@ import Header from './Components/Header';
 import CreateResume from './pages/CreateResume';
 import Footer from './Components/Footer';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter, Link } from "react-router-dom";
+
 
 const GetResume = () => {
   return (
     <div className="GetResume">
       <Header />
-        <Router>
-          <Route path="/getresume.io" exact component={App} />
-          <Route path="/getresume.io/login" exact component={LoginActivity} />
-          <Route path="/getresume.io/signup" exact component={SignupActivity} />
-          <Route path="/getresume.io/create-resume" exact component={CreateResume} />
-          {/* Setting up the 404 PAGE NOT FOUND module using Routes */}
-          {/* <Route component={PageNotFound} /> */}
-        </Router>
+        <HashRouter basename="/">
+          <Route path="/" exact component={App} />
+          <Route path="/login" exact component={LoginActivity} />
+          <Route path="/signup" exact component={SignupActivity} />
+          <Route path="/create-resume" exact component={CreateResume} />
+          <Route path="/profile" exact component />
+        </HashRouter>
+          
       <Footer />
     </div>
   )
